cbb634a93897558dc66ac92557d2f765e34a3096
Moni <thaker.moni@gmail.com>
2024-05-29 02:35:25 +1200

    build: Configured bot login
    Ranking bot can now successfully log into Discord via node.
    
    Also set up the required code for discord.js events and commands.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv").config;
+require("dotenv").config();
 
 const fs = require("node:fs");
 const path = require("node:path");
@@ -18,3 +18,43 @@ const client = new Client({
     GatewayIntentBits.GuildMessageReactions,
   ],
 });
+
+// Load event files on startup
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
+
+// Load command files on startup
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(
+      `Error. The command at ${filePath} is missing a required "data" or "execute" property.`
+    );
+  }
+}
+
+client.login(token);
